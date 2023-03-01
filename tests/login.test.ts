0518c6395d545c7d6762f86054e8a7b5956ff3c4
Muhammad Ahsin <ahsin.and@gmail.com>
2023-03-01 15:15:29 +0700

    authentication test

--- tests/login.test.ts
+++ tests/login.test.ts
@@ -7,8 +7,8 @@ const request = supertest(app);
 const emailTest = 'admin@mail.com';
 const passwordTest = 'myPassword';
 
-let userData = null;
-describe('Login endpoint', () => {
+let token: string = '';
+describe('Auth Test', () => {
   it('POST Login', (done) => {
     const request = supertest(app)
       .post('/login')
@@ -21,7 +21,24 @@ describe('Login endpoint', () => {
         if (!('token' in res.body.data)) throw new Error('The token attribute was not found in result');
 
         if (res.body.data) {
-          userData = res.body.data;
+          token = res.body.data.token;
+        }
+      })
+      .end(function (err, res) {
+        if (err) return done(err);
+        return done();
+      });
+  });
+
+  it('Get logged in user', (done) => {
+    const request = supertest(app)
+      .get('/auth/me')
+      .auth(token, { type: 'bearer' })
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .expect((res) => {
+        if (res.body.data.email !== emailTest) {
+          throw new Error('The email was not found');
         }
       })
       .end(function (err, res) {
@@ -30,5 +47,3 @@ describe('Login endpoint', () => {
       });
   });
 });
-
-console.log(userData);
